93ab437016c243411771b6b0910c659abbebe974
Dwyer Deighan <dwyerdei@buffalo.com>
2024-08-13 18:49:42 -0400

    fixed the low-pass filter to use consistent frequencies in niche cases

--- MOR_Operator.py
+++ MOR_Operator.py
@@ -19,25 +19,31 @@ class MOR_Layer(BasicLightningRegressor):
     def forward(self, u):
         u = torch.as_tensor(u).to(self.device)
         assert len(u.shape)==2+self.ndims # +1 for batch, +1 for channels
+        # u.shape==[batch, in_channels, x, y, ...]
 
+        # Convert to complex dtype (makes shape "correct")
+        g = torch.view_as_complex(self.g_mode_params)
+
+        # Pad fft_shape to make it compatible with simple low-pass code below
+        fft_shape = [u_s + abs(u_s-g_s)%2 for u_s, g_s in zip(u.shape[-self.ndims:], g.shape[-self.ndims:])]
         fft_dims = list(range(-self.ndims, 0))
         # Apply Fourier transform (last ndims need to be spatial!)
 
-        # should FFT the last self.ndims modes
-        u_fft = torch.fft.fftn(u, dim=fft_dims)
+        # should FFT the last self.ndims modes, also we pad (if needed) to low-pass work
+        u_fft = torch.fft.fftn(u, s=fft_shape, dim=fft_dims)
         u_fft = torch.fft.fftshift(u_fft, dim=fft_dims)
 
-        # Convert to complex dtype & pad the g_mode_params (this will drop extra modes)
-        g = torch.view_as_complex(self.g_mode_params)
+        # Pad the g_mode_params (this will drop extra modes)
         g_padded_shape = list(g.shape[:-self.ndims])+list(u_fft.shape[-self.ndims:]) # fuse shapes
         g_padded = torch.zeros(*g_padded_shape, dtype=g.dtype, device=g.device)
 
         # get total modes to drop for each dimension
-        modes_to_drop = np.asarray(u.shape[-self.ndims:]) - np.asarray(g.shape[-self.ndims:])
+        modes_to_drop = np.asarray(u_fft.shape[-self.ndims:]) - np.asarray(g.shape[-self.ndims:])
+        assert (modes_to_drop%2==0).all() # i.e. compatible parity
+        assert (modes_to_drop>=0).all() # i.e. input bigger than low-pass modes
 
         # 1st part selects input & output channel dimensions, 2nd part inserts g in the center (at the low_freqs)
-        # GOTCHA: this only works when the g is odd & the padding is even, otherwise it fails!
-        low_pass_slices = [slice(None)]*2 + [slice(s_i//2+s_i%2, -s_i//2 if s_i>0 else None) for s_i in modes_to_drop]
+        low_pass_slices = [slice(None)]*2 + [slice(s_i//2, -s_i//2 if s_i>0 else None) for s_i in modes_to_drop]
         g_padded[low_pass_slices] = g
         g_padded = g_padded[None] # add batch dimension
 
@@ -48,8 +54,8 @@ class MOR_Layer(BasicLightningRegressor):
 
         # Apply inverse Fourier transform
         u_fft = torch.fft.ifftshift(u_fft, dim=fft_dims)
-        u_ifft = torch.fft.ifftn(u_fft, dim=fft_dims)
-        # should IFFT the last self.ndims modes
+        u_ifft = torch.fft.ifftn(u_fft, s=u.shape[-self.ndims:], dim=fft_dims)
+        # should IFFT the last self.ndims modes, also crop/pad to original shape
 
         # Apply point-wise MLP nonlinearity h(u)
         hu_ifft = self.h_mlp(u_ifft.real)
