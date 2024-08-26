48ee35e392a9304f610f190d5086db855489c0ab
Dwyer Deighan <dwyerfire@gmail.com>
2024-08-26 12:53:09 -0600

    deleted the uselss L2 regularization

--- POU_net.py
+++ POU_net.py
@@ -29,26 +29,21 @@ class FieldGatingNet(BasicLightningRegressor):
         pos_encodings = self._make_positional_encodings(X.shape[-self.n_inputs:])
         return self._gating_net(pos_encodings)
 
-# NOTE: The new POU_net should actually generalize the original!
-# We only exported the gating net & the miracle is that the forward method still works!!
 class POU_net(BasicLightningRegressor):
+    ''' POU_net minus the L2 regularization (which is useless). '''
     def __init__(self, make_expert=MOR_Operator.MOR_Operator, n_experts=5, n_inputs=2,
-                 make_gating_net: type=FieldGatingNet, lr=0.001, T_max=25,
-                 L2_weight=0.0, L2_patience=10, L2_decay=0.9):
+                 make_gating_net: type=FieldGatingNet, lr=0.001, T_max=25):
         super().__init__()
         # NOTE: setting n_experts=n_experts+1 inside the gating_net implicitly adds a "ZeroExpert"
         self.gating_net=make_gating_net(n_inputs=n_inputs, n_experts=n_experts+1) # supports n_inputs!=2
         self.experts=nn.ModuleList([make_expert() for i in range(n_experts)])
         vars(self).update(locals()); del self.self
 
-    # TODO: delete L2 regularization when using MFVI (use prior instead)
     def configure_optimizers(self):
-        param_groups = [{'params': self.experts.parameters(), 'weight_decay': self.L2_weight},
-                        {'params': self.gating_net.parameters()}] #L2 for experts only
-        self.experts_optim = torch.optim.Adam(param_groups, lr=self.lr)
-        lr_schedule = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(self.experts_optim, T_0=self.T_max, T_mult=2)
-        #lr_schedule=torch.optim.lr_scheduler.CosineAnnealingLR(self.experts_optim, T_max=self.T_max)
-        return [self.experts_optim], [lr_schedule]
+        optim = torch.optim.Adam(self.parameters(), lr=self.lr)
+        lr_schedule = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optim, T_0=self.T_max, T_mult=2)
+        #lr_schedule=torch.optim.lr_scheduler.CosineAnnealingLR(optim, T_max=self.T_max)
+        return [optim], [lr_schedule]
 
     # Verified to work 7/19/24
     def forward(self, X):
@@ -58,24 +53,3 @@ class POU_net(BasicLightningRegressor):
         for i, expert in enumerate(self.experts):
             prediction = prediction + gating_weights[:,i]*expert(X)
         return prediction
-
-    def on_train_start(self):
-        self._lowest_loss = float('inf') # for L2 patience
-        self._n_stag_steps = 0
-
-    # Verified to work 7/19/24
-    def _decay_L2(self, loss):
-        """ Decay L2 like original POU_net, but maybe not necessary for Operator MoE? """
-        if self._lowest_loss>loss.item():
-            self._lowest_loss=loss.item()
-            self._n_stag_steps=0
-        else:
-            self._n_stag_steps += 1
-            if self._n_stag_steps%self.L2_patience==0:
-                self.experts_optim.param_groups[0]['weight_decay'] *= self.L2_decay
-        self.log('L2_coef', float(self.experts_optim.param_groups[0]['weight_decay']), prog_bar=True)
-
-    def training_step(self, batch, batch_idx):
-        loss = super().training_step(batch)
-        self._decay_L2(loss)
-        return loss
