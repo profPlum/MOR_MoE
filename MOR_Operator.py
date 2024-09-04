fixed param initialization to constant variance, now the rfft implementation is just as good as the original

MOR_Operator.py
@@ -36,10 +36,11 @@ class MOR_Layer(BasicLightningRegressor):
                 k_modes = ndims*[k_modes]
         def make_g(in_channels, out_channels):
             g_shape = [in_channels, out_channels]+k_modes[:ndims-1] + [k_modes[-1]//2+1, 2]
-            #g_shape = [in_channels, out_channels]+k_modes + [2]
-            scale = 1.0#/(in_channels**0.5)#out_channels) # similar to xavier initializaiton
+            scale = 5e-3 #1.0/(in_channels**0.5)#out_channels) # similar to xavier initializaiton
             param = torch.randn(*g_shape)*scale
-            param = torch.nn.init.xavier_uniform_(param)
+            #torch.nn.init.kaiming_normal_(param, nonlinearity='relu')
+            #torch.nn.init.xavier_uniform_(param)
+            #print(f'{param.std().item()}')
             return nn.Parameter(param)
 
         # Define the weights in the Fourier domain (complex values)