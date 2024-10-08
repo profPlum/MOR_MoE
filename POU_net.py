fixing problems with VI where kl weight was incorrect

POU_net.py
@@ -182,7 +182,7 @@ class POU_net(L.LightningModule):
         metrics = self.val_metrics if val else self.train_metrics
 
         # simple helper does everything needed to log one metric!
-        def log_metric(name, metric=None, on_step=val, on_epoch=True, **kwd_args):
+        def log_metric(name, metric=None, on_step=False, on_epoch=True, **kwd_args):
             if metric is None: metric = getattr(metrics, name)
             if on_step: metric(y_pred, y) # update metric
             else: metric.update(y_pred, y)
@@ -205,12 +205,12 @@ class POU_net(L.LightningModule):
 import model_agnostic_BNN
 
 class PPOU_net(POU_net): # Not really, it's POU+VI
-    def __init__(self, n_inputs, n_outputs, *args, prior_cfg={}, **kwd_args):
+    def __init__(self, n_inputs, n_outputs, train_dataset, *args, prior_cfg={}, **kwd_args):
         # we double output channels to have the sigma predictions too
         super().__init__(n_inputs*2, n_outputs*2, *args, **kwd_args)
 
         # make VI reparameterize our entire model
-        model_agnostic_BNN.model_agnostic_dnn_to_bnn(self, prior_cfg=prior_cfg)
+        model_agnostic_BNN.model_agnostic_dnn_to_bnn(self, train_dataset, prior_cfg=prior_cfg)
 
     def forward(self, X, Y=None):
         if Y is None: Y = torch.zeros(1,device=X.device, dtype=X.dtype).expand(*X.shape)
@@ -224,8 +224,8 @@ class PPOU_net(POU_net): # Not really, it's POU+VI
         X, y = batch
         y_pred_mu, y_pred_sigma = self(X)
 
-        num_batches = self.trainer.estimated_stepping_batches//self.trainer.max_epochs # sneakily extract from PL
-        kl_loss = self.get_kl_loss()/(num_batches*y.numel()) # (weighted)
+        #num_batches = len(self.trainer.train_dataloader.dataset)//self.trainer.train_dataloader.batch_size # sneakily extract from PL
+        kl_loss = self.get_kl_loss()#/(num_batches*y.numel()) # (weighted)
         loss = model_agnostic_BNN.nll_regression(y_pred_mu, y, y_pred_sigma=y_pred_sigma, reduction=torch.mean) + kl_loss # posterior loss
 
         self.log(f'{val*"val_"}loss', loss.item(), sync_dist=val, prog_bar=True)

channel.py
@@ -23,7 +23,7 @@ one_cycle=bool(int(os.environ.get('ONE_CYCLE', False))) # scheduler
 three_phase=bool(int(os.environ.get('THREE_PHASE', False))) # adds decay after inital bump
 RLoP=bool(int(os.environ.get('RLoP', False))) # scheduler
 RLoP_factor=0.9
-RLoP_patience=25
+RLoP_patience=15
 
 # Import External Libraries
 import os
@@ -56,18 +56,18 @@ class MemMonitorCallback(L.Callback):
 
 # wrapper to nullify the prior_cfg kwd_arg (for compatibility)
 class POU_NetSimulator(POU_NetSimulator):
-    def __init__(self, *args, prior_cfg={}, **kwd_args):
+    def __init__(self, *args, prior_cfg={}, train_dataset=None, **kwd_args):
         super().__init__(*args, **kwd_args)
 
 if __name__=='__main__':
     # setup dataset
     dataset = JHTDB_Channel('data/turbulence_output', time_chunking=time_chunking)
-    dataset_long_horizon = JHTDB_Channel('data/turbulence_output', time_chunking=time_chunking*2)
+    dataset_long_horizon = JHTDB_Channel('data/turbulence_output', time_chunking=time_chunking*3)
     _, val_long_horizon = torch.utils.data.random_split(dataset, [0.8, 0.2])
     train_dataset, val_dataset = torch.utils.data.random_split(dataset, [0.8, 0.2])
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, num_workers=16, pin_memory=True)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, num_workers=8)
-    #val_long_loader = torch.utils.data.DataLoader(val_long_horizon, batch_size=batch_size, num_workers=8)
+    val_long_loader = torch.utils.data.DataLoader(val_long_horizon, batch_size=batch_size, num_workers=8)
     print(f'{len(dataset)=}\n{len(train_loader)=}\n{len(val_dataset)=}')
 
     IC_0, Sol_0 = dataset[0]
@@ -87,7 +87,7 @@ if __name__=='__main__':
     if scale_lr: lr *= num_nodes
     model = SimModelClass(n_inputs=ndims, n_outputs=ndims, n_experts=n_experts, ndims=ndims, lr=lr, make_optim=make_optim, T_max=T_max,
                           one_cycle=one_cycle, three_phase=three_phase, RLoP=RLoP, RLoP_factor=RLoP_factor, RLoP_patience=RLoP_patience,
-                          n_steps=time_chunking-1, k_modes=k_modes, prior_cfg={'prior_sigma': prior_sigma})
+                          n_steps=time_chunking-1, k_modes=k_modes, prior_cfg={'prior_sigma': prior_sigma}, train_dataset=train_dataset)
 
     import os, signal
     from pytorch_lightning.loggers import TensorBoardLogger
@@ -97,9 +97,8 @@ if __name__=='__main__':
     logger = TensorBoardLogger("lightning_logs", name=os.environ.get("SLURM_JOB_NAME", 'JHTDB_MOR_MoE'),
                                 version=os.environ.get("SLURM_JOB_ID", None))
     profiler = L.profilers.PyTorchProfiler(profile_memory=True, with_stack=True,
-                                           on_trace_ready=torch.profiler.tensorboard_trace_handler(logger.log_dir))
-                                           #schedule=torch.profiler.schedule(wait=2, warmup=2, active=6, repeat=0))
-                                           ## TODO: remove when done debugging
+                                           on_trace_ready=torch.profiler.tensorboard_trace_handler(logger.log_dir),
+                                           schedule=torch.profiler.schedule(wait=12, warmup=2, active=6, repeat=3))
 
     # This is needed to avoid problem caused by large model size
     model_checkpoint_callback=L.callbacks.ModelCheckpoint(save_weights_only=True, monitor='loss')
@@ -108,5 +107,6 @@ if __name__=='__main__':
                         gradient_clip_val=gradient_clip_val, gradient_clip_algorithm='value', # regularization isn't good for OneCycleLR
                         profiler=profiler, logger=logger, plugins=[SLURMEnvironment()], callbacks=[model_checkpoint_callback])
 
-    val_dataloaders = [val_loader] #, val_long_loader] # long validation loader causes various problems with profiler & GPU utilization...
+    val_dataloaders = [val_loader, val_long_loader] # long validation loader causes various problems with profiler & GPU utilization...
     trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_dataloaders) #, ckpt_path=ckpt_path)
+    #trainer.validate(model=model, dataloaders=val_dataloaders) #, ckpt_path=ckpt_path)

model_agnostic_BNN.py
@@ -2,6 +2,7 @@ import os, random
 import torch, numpy as np
 from torch import nn
 import torch.nn.utils.parametrize as parametrize
+import warnings
 
 # NOTE: We changed it to use sum because the prior p(theta)=p(theta_0)p(theta_1)...p(theta_n) is a product of gaussians
 # which means that more parameters will increase the KL loss. Also KL divergence takes a log which implies the sum.
@@ -117,7 +118,8 @@ class _BayesianParameterization(nn.Module):
         return self._kl_loss
 
 # NOTE: this is essentially the new dnn_to_bnn() but also more versatile
-def model_agnostic_dnn_to_bnn(dnn: nn.Module, prior_cfg: dict = {}):
+# NOTE: if you pass in train_dataset and it is non-sparse (e.g. floating point regression) then it will automatically weight the get_kl_loss method!
+def model_agnostic_dnn_to_bnn(dnn: nn.Module, train_dataset: torch.utils.data.Dataset=None, prior_cfg: dict = {}):
     if 'Bayesian' in type(dnn).__name__: return
 
     # apply _BayesianParameterization
@@ -134,7 +136,14 @@ def model_agnostic_dnn_to_bnn(dnn: nn.Module, prior_cfg: dict = {}):
     def forward(self, *args, **kwargs):
         with parametrize.cached():
             return super(type(self), self).forward(*args, **kwargs)
-    methods =  {'forward': forward, 'get_kl_loss': get_kl_loss}
+    kl_weight=1.0
+    if train_dataset:
+        X, y = train_dataset[0]
+        assert torch.is_floating_point(y) # classification datasets are implicitly sparse which makes this fail...
+        kl_weight = 1.0/(len(train_dataset)*y.numel())
+    else: warnings.warn("you didn't pass in the train_dataset so get_kl_loss() values will be unweighted! (make sure to weight them yourself)")
+    _get_kl_loss = lambda self: get_kl_loss(self)*kl_weight
+    methods =  {'forward': forward, 'get_kl_loss': _get_kl_loss}
     dnn.__class__  = type(f'Bayesian{dnn.__class__.__name__}', (type(dnn),), methods)
     return dnn
 