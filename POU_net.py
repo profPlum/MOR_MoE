Major increase to recursive steps 5-->8, and fixed theoretical correctness of variance summation

POU_net.py
@@ -198,30 +198,42 @@ class POU_net(L.LightningModule):
 
 import model_agnostic_BNN
 
-class ProbabilisticExpertWrapper(MOR_Operator.MOR_Operator):
-    def forward(self, X):
-        pred = super().forward(X)
-        constrainted_preds = torch.empty_like(pred)
-        constrainted_preds[:, :pred.shape[1]//2] = pred[:, :pred.shape[1]//2]
-        constrainted_preds[:, pred.shape[1]//2:] = F.softplus(pred[:, pred.shape[1]//2:])**2 # make pred use variance
-        return constrainted_preds
-
 class PPOU_net(POU_net): # Not really, it's POU+VI
     def __init__(self, n_inputs, n_outputs, train_dataset_size, *args, prior_cfg={}, **kwd_args):
         # we double output channels to have the sigma predictions too
-        super().__init__(n_inputs*2, n_outputs*2, *args, make_expert=ProbabilisticExpertWrapper, **kwd_args)
+        super().__init__(n_inputs*2, n_outputs*2, *args, **kwd_args)
 
         # make VI reparameterize our entire model
         model_agnostic_BNN.model_agnostic_dnn_to_bnn(self, train_dataset_size, prior_cfg=prior_cfg)
 
+    def forward(self, X, Y=None):
+        ''' crazy forward method that does everything needed for variance summation '''
+        X = torch.as_tensor(X).to(self.device)
+        if Y is None: Y = torch.zeros(1,device=X.device, dtype=X.dtype).expand(*X.shape)
+        X = torch.cat([X,Y], axis=1)
+
+        gating_weights, topk = self.gating_net(X)
+        prediction_mu = 0
+        prediction_sigma_sq = 0
+        for i, k_i in enumerate(topk):
+            pred_i = self.experts[k_i](X)
+            mu = pred_i[:, :pred_i.shape[1]//2]
+            sigma = F.softplus(pred_i[:, pred_i.shape[1]//2:]) # make pred use variance
+
+            prediction_mu = prediction_mu + gating_weights[:,i:i+1]*mu
+            prediction_sigma_sq = prediction_sigma_sq + (gating_weights[:,i:i+1]*sigma)**2
+        return prediction_mu, prediction_sigma_sq**0.5
+
+''' # original forward before probabilistic considerations
     def forward(self, X, Y=None):
         if Y is None: Y = torch.zeros(1,device=X.device, dtype=X.dtype).expand(*X.shape)
         X = torch.cat([X,Y], axis=1)
         pred = super().forward(X)
 
         mu_pred = pred[:, :pred.shape[1]//2]
-        sigma_sq_pred = pred[:, pred.shape[1]//2:]
-        return mu_pred, sigma_sq_pred**0.5
+        sigma_pred = F.softplus(pred[:, pred.shape[1]//2:])
+        return mu_pred, sigma_pred
+'''
 
     def training_step(self, batch, batch_idx=None, val=False):
         X, y = batch

channel.py
@@ -7,7 +7,7 @@ from torch.optim import lr_scheduler
 
 k_modes=[103,26,77] # can be a list
 n_experts: int=2 # number of experts in MoE
-time_chunking: int=5 # how many self-aware recursive steps to take
+time_chunking: int=8 # how many self-aware recursive steps to take
 batch_size: int=1 # batch size, with VI experts we can only fit 1 batch on 20 GPUs!
 scale_lr=True # scale with DDP batch_size
 lr: float=float(os.environ.get('LR', 1.25e-4)) # learning rate