be0ae1425f5fb9ba26f840b1c4fda6681bb34f34
Dwyer Deighan <dwyerfire@gmail.com>
2025-05-08 15:28:40 -0500

    Discovered that x2 layers actually results in > x2 parameters b/c of projection layers. Then rectified the problem with N_LAYERS=4+2 & N_FILTERS=32+1

--- experiment_configs/control.sh
+++ experiment_configs/control.sh
@@ -1,3 +1,5 @@
-export TIME_CHUNKING=2 # 9 is max (but only verified to work for 10 at least nodes)
-export N_LAYERS=8 # number of expert layers
-export N_EXPERTS=1 # number of experts
+# These settings disable everything i.e. auto regressive timestepping & mixture of experts (which is to say it replaces mixture with one big expert)
+export TIME_CHUNKING=2 # 9 is default (& the max for 10x A100 GPUs), 2 is the minimum (i.e. it disables auto-regressive timestepping feature)
+export N_LAYERS=6 # number of expert layers, default_params = 2*proj_layer + 2*full_layer, full_layer >> proj_layer so we double that = 2*proj_layer + 4*full_layer = 6 layers
+export N_FILTERS=33 # (default=32) + 1 to correct for not doubling projection layers (works almost perfectly!)
+export N_EXPERTS=1 # number of experts specifying one implies not even the zero expert is used
--- experiment_configs/recursive_steps.sh
+++ experiment_configs/recursive_steps.sh
@@ -1,2 +1,4 @@
-export N_LAYERS=8 # number of expert layers
-export N_EXPERTS=1 # number of experts
+# these settings relpace mixture of experts with one big expert
+export N_LAYERS=6 # number of expert layers, default_params = 2*proj_layer + 2*full_layer, full_layer >> proj_layer so we double that = 2*proj_layer + 4*full_layer = 6 layers
+export N_FILTERS=33 # (default=32) + 1 to correct for not doubling projection layers (works almost perfectly!)
+export N_EXPERTS=1 # number of experts specifying one implies not even the zero expert (or gating network) is used (i.e. disable mixture of experts)
