125957b784d42f3497fe0a74f68e346d0221133e
Dwyer Deighan <dwyerfire@gmail.com>
2025-05-16 18:53:36 -0500

    updated experiment_configs so that the no recursive steps case still uses a similar amount of ground-truth data per optimization step

--- experiment_configs/control.sh
+++ experiment_configs/control.sh
@@ -3,3 +3,4 @@ export TIME_CHUNKING=2 # 9 is default (& the max for 10x A100 GPUs), 2 is the mi
 export N_LAYERS=6 # number of expert layers, default_params = 2*proj_layer + 2*full_layer, full_layer >> proj_layer so we double that = 2*proj_layer + 4*full_layer = 6 layers
 export N_FILTERS=33 # (default=32) + 1 to correct for not doubling projection layers (works almost perfectly!)
 export N_EXPERTS=1 # number of experts specifying one implies not even the zero expert is used
+export BATCH_SIZE=13 # really should be 13.5 but whatever bigger b/c we removed the recursive steps
--- experiment_configs/mixture_of_experts.sh
+++ experiment_configs/mixture_of_experts.sh
@@ -1,1 +1,2 @@
 export TIME_CHUNKING=2 # disable recursive steps
+export BATCH_SIZE=13 # really should be 13.5 but whatever bigger b/c we removed the recursive steps
