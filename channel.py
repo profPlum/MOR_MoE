d73dea482a5b6ef768cc977942ca2ab7dcdcb5ef
Dwyer Deighan <dwyerfire@gmail.com>
2025-09-13 05:34:37 -0500

    added stride+Batch settings to make Everything run efficiently on just one GPU

--- channel.py
+++ channel.py
@@ -94,7 +94,7 @@ if __name__=='__main__':
     _, val_long_horizon = torch.utils.data.random_split(dataset_long_horizon, [0.5, 0.5]) # 50% ensures there are two validation steps
     val_long_horizon = preload_dataset(val_long_horizon) # pre-load only the subset we use
     train_dataset, val_dataset = torch.utils.data.random_split(dataset, [0.8, 0.2])
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, pin_memory=True, shuffle=True, drop_last=True)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size*long_horizon_multiplier)
     val_long_loader = torch.utils.data.DataLoader(val_long_horizon, batch_size=batch_size)
     print(f'{len(dataset)=}\n{len(train_loader)=}\n{len(val_dataset)=}')
