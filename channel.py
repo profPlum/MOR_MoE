735bbeb05d08d1b9545844dfe998d3f37273d260
Dwyer Deighan <dwyerfire@gmail.com>
2025-09-15 00:34:29 -0500

    increased precision of parameter count

--- channel.py
+++ channel.py
@@ -136,7 +136,7 @@ if __name__=='__main__':
                           lr=lr, T_max=T_max, one_cycle=one_cycle, three_phase=three_phase, RLoP=RLoP, RLoP_factor=RLoP_factor, RLoP_patience=RLoP_patience,
                           n_steps=time_chunking-1, trig_encodings=use_trig, make_gating_net=EqualizedFieldGatingNet, simulator=simulator, **optional_kwd_args)
 
-    print(f'num model parameters: {utils.count_parameters(model):.2e}')
+    print(f'num model parameters: {utils.count_parameters(model):.5e}')
     print('model:')
     print(model)
 
