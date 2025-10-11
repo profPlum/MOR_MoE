0da65975e9217feec8f82bcfe4de7286562eac04
Dwyer Deighan <dwyerfire@gmail.com>
2025-10-10 21:56:45 -0500

    added official configs for WNO comparison

---
+++ experiment_configs/WNO.sh
@@ -0,0 +1,7 @@
+# WNO3d experts?
+export USE_WNO3D_EXPERTS=1 # To use WNO3d experts: set USE_WNO3D_EXPERTS=1 and USE_CNN_EXPERTS=0
+export WNO3D_LEVEL=1 # wavelet decomposition level (default 2, higher = more detailed wavelet analysis)
+export N_EXPERTS=1 N_LAYERS=4 OUT_NORM_GROUPS=0
+export STRIDE='[1.609,1.625,1.203]' FAST_DATALOADERS=1
+export MAX_EPOCHS=800
+export BATCH_SIZE=2
--- experiment_configs/everything_for_WNO_comparison.sh
+++ experiment_configs/everything_for_WNO_comparison.sh
@@ -1,7 +1,12 @@
 # these are already the defaults but written explicitly
 export N_LAYERS=4 # number of expert layers
-export N_FILTERS=32 # number of hidden filters (aka channel size)
 export N_EXPERTS=3 # number of experts
 export BATCH_SIZE=2 # other configs assume default is 2 so lets make it explicit
 export TIME_CHUNKING=10 # other configs assume default is 10 so lets make it explicit
 export USE_NORMALIZED_MOE=1 # importance normalization
+
+export USE_WNO3D_EXPERTS=0 # To use WNO3d experts: set USE_WNO3D_EXPERTS=1 and USE_CNN_EXPERTS=0
+export STRIDE='[1.609,1.625,1.203]' FAST_DATALOADERS=1 # 64x16x64
+export MAX_EPOCHS=800
+export N_FILTERS=36
+
