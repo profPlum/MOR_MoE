69907645cc5c63a5bd1dea2d6c07123710289130
Dwyer Deighan <dwyerdei@buffalo.com>
2026-08-20 20:56:14 -0400

    updated every reference to N_LAYERS=N_LAYERS+1 for MOR and N_LAYERS=N_LAYERS+2 for WNO for with (correct/consistent) interface

--- experiment_configs/mixture_of_experts_normalized.sh
+++ experiment_configs/mixture_of_experts_normalized.sh
@@ -1,5 +1,5 @@
 # these are already the defaults but written explicitly
-export N_LAYERS=4 # number of expert layers
+export N_LAYERS=5 # number of expert layers
 export N_FILTERS=32 # number of hidden filters (aka channel size)
 export N_EXPERTS=3 # number of experts
 
